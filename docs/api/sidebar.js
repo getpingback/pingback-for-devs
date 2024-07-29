4e93c4510a71ac2de286d54a8f8ed46465227a0f
Pedro Ladeira <pmladeira@live.com>
2024-07-29 15:19:01 -0300

    feat(api): segmentations page and add subscriber to a segmentation list

--- docs/api/sidebar.js
+++ docs/api/sidebar.js
@@ -1,1 +1,26 @@
-module.exports = [{"type":"doc","id":"api/introduction"},{"type":"category","label":"Subscribers","link":{"type":"doc","id":"api/subscribers"},"items":[{"type":"doc","id":"api/create-subscriber","label":"Create a subscriber","className":"api-method post"},{"type":"doc","id":"api/get-subscriber","label":"Get a subscriber","className":"api-method get"},{"type":"doc","id":"api/update-subscriber","label":"Update a subscriber","className":"api-method put"}]}];
+module.exports = [
+  { type: "doc", id: "api/introduction" },
+  {
+    type: "category",
+    label: "Subscribers",
+    link: { type: "doc", id: "api/subscribers" },
+    items: [
+      { type: "doc", id: "api/create-subscriber", label: "Create a subscriber", className: "api-method post" },
+      { type: "doc", id: "api/get-subscriber", label: "Get a subscriber", className: "api-method get" },
+      { type: "doc", id: "api/update-subscriber", label: "Update a subscriber", className: "api-method put" },
+    ],
+  },
+  {
+    type: "category",
+    label: "Segmentations",
+    link: { type: "doc", id: "api/segmentations" },
+    items: [
+      {
+        type: "doc",
+        id: "api/add-subscriber-to-segment",
+        label: "Add a subscriber to a segmentation list",
+        className: "api-method post",
+      },
+    ],
+  },
+];
